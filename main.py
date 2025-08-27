6e7016c24be2ea1bf83682a29318f1129637f322
12liyab <ktliyab7@gmail.com>
2025-08-27 23:23:39 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 print("Hello World")
 print("Liyab is my name")
 print("This is  my first python code")
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Load the dataset
+df = pd.read_csv('stocks (1).csv')
+
+# Inspect the data
+print(df.head())
+print(df.info())
